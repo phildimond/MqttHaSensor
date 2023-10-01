d9935c2944078ea4677ea982129b98d50dbe7f0c
Phillip Dimond <phil@adgaming.com.au>
2023-10-01 21:53:14 +1100

    Added deep sleep & cleaned up code.

--- main/main.c
+++ main/main.c
@@ -13,22 +13,20 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_spiffs.h"
+#include "esp_sleep.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
 #include "utilities.h"
 #include "config.h"
 
 #define DEBUG 1
+#define SLEEPTIME 15
+#define S_TO_uS(s) (s * 1000000)
 
 // static const char *TAG = "example";
 
 void app_main(void)
 {
-    /*
-    char input[80];
-    printf("Enter a string: ");
-    int length = getLineInput(input, 80);
-    printf("\r\nYou entered the %d character string %s\r\n", length, input);
-    */
-
     // Initialise the SPIFFS system
     esp_vfs_spiffs_conf_t conf = {
         .base_path = "/spiffs",
@@ -41,18 +39,6 @@ void app_main(void)
         printf("SPIFFS Mount Failed: %s\r\n", esp_err_to_name(ret));
         printf("Reformatting the SPIFFs partition, please restart.");
         return;
-    }
-
-    size_t total = 0, used = 0;
-    ret = esp_spiffs_info(conf.partition_label, &total, &used);
-    if (ret != ESP_OK)
-    {
-        printf("Failed to get SPIFFS partition information (%s).\r\n", esp_err_to_name(ret));
-        return;
-    }
-    else
-    {
-        printf("Partition size: total: %d, used: %d\r\n", total, used);
     }
 
     // Load the configuration from the file system
@@ -83,116 +69,26 @@ void app_main(void)
         printf("               WiFi SSID: %s, WiFi Password: %s\r\n", config.ssid, config.pass);
         printf("               MQTT URL: %s, Username: %s, Password: %s\r\n", config.mqttBrokerUrl, config.mqttUsername, config.mqttPassword);
     }
-    /*
-
-        ESP_LOGI(TAG, "Initializing SPIFFS");
-
-        esp_vfs_spiffs_conf_t conf = {
-          .base_path = "/spiffs",
-          .partition_label = NULL,
-          .max_files = 5,
-          .format_if_mount_failed = true
-        };
-
-        // Use settings defined above to initialize and mount SPIFFS filesystem.
-        // Note: esp_vfs_spiffs_register is an all-in-one convenience function.
-        esp_err_t ret = esp_vfs_spiffs_register(&conf);
-
-        if (ret != ESP_OK) {
-            if (ret == ESP_FAIL) {
-                ESP_LOGE(TAG, "Failed to mount or format filesystem");
-            } else if (ret == ESP_ERR_NOT_FOUND) {
-                ESP_LOGE(TAG, "Failed to find SPIFFS partition");
-            } else {
-                ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
-            }
-            return;
-        }
-
-    #ifdef CONFIG_EXAMPLE_SPIFFS_CHECK_ON_START
-        ESP_LOGI(TAG, "Performing SPIFFS_check().");
-        ret = esp_spiffs_check(conf.partition_label);
-        if (ret != ESP_OK) {
-            ESP_LOGE(TAG, "SPIFFS_check() failed (%s)", esp_err_to_name(ret));
-            return;
-        } else {
-            ESP_LOGI(TAG, "SPIFFS_check() successful");
-        }
-    #endif
-
-        size_t total = 0, used = 0;
-        ret = esp_spiffs_info(conf.partition_label, &total, &used);
-        if (ret != ESP_OK) {
-            ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s). Formatting...", esp_err_to_name(ret));
-            esp_spiffs_format(conf.partition_label);
-            return;
-        } else {
-            ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
-        }
-
-        // Check consistency of reported partiton size info.
-        if (used > total) {
-            ESP_LOGW(TAG, "Number of used bytes cannot be larger than total. Performing SPIFFS_check().");
-            ret = esp_spiffs_check(conf.partition_label);
-            // Could be also used to mend broken files, to clean unreferenced pages, etc.
-            // More info at https://github.com/pellepl/spiffs/wiki/FAQ#powerlosses-contd-when-should-i-run-spiffs_check
-            if (ret != ESP_OK) {
-                ESP_LOGE(TAG, "SPIFFS_check() failed (%s)", esp_err_to_name(ret));
-                return;
-            } else {
-                ESP_LOGI(TAG, "SPIFFS_check() successful");
-            }
-        }
-
-        // Use POSIX and C standard library functions to work with files.
-        // First create a file.
-        ESP_LOGI(TAG, "Opening file");
-        FILE* f = fopen("/spiffs/hello.txt", "w");
-        if (f == NULL) {
-            ESP_LOGE(TAG, "Failed to open file for writing");
-            return;
-        }
-        fprintf(f, "Hello World!\n");
-        fclose(f);
-        ESP_LOGI(TAG, "File written");
-
-        // Check if destination file exists before renaming
-        struct stat st;
-        if (stat("/spiffs/foo.txt", &st) == 0) {
-            // Delete it if it exists
-            unlink("/spiffs/foo.txt");
-        }
-
-        // Rename original file
-        ESP_LOGI(TAG, "Renaming file");
-        if (rename("/spiffs/hello.txt", "/spiffs/foo.txt") != 0) {
-            ESP_LOGE(TAG, "Rename failed");
-            return;
-        }
-
-        // Open renamed file for reading
-        ESP_LOGI(TAG, "Reading file");
-        f = fopen("/spiffs/foo.txt", "r");
-        if (f == NULL) {
-            ESP_LOGE(TAG, "Failed to open file for reading");
-            return;
-        }
-        char line[64];
-        fgets(line, sizeof(line), f);
-        fclose(f);
-        // strip newline
-        char* pos = strchr(line, '\n');
-        if (pos) {
-            *pos = '\0';
-        }
-        ESP_LOGI(TAG, "Read from file: '%s'", line);
-
-        // All done, unmount partition and disable SPIFFS
-        esp_vfs_spiffs_unregister(conf.partition_label);
-        ESP_LOGI(TAG, "SPIFFS unmounted");
-    */
 
     // All done, unmount partition and disable SPIFFS
     esp_vfs_spiffs_unregister(conf.partition_label);
     printf("SPIFFS unmounted.\r\n");
+
+    // Go to sleep
+    if (DEBUG) { printf("Time to sleep for %d seconds.\r\n", SLEEPTIME); }
+    if (esp_sleep_enable_timer_wakeup(S_TO_uS(SLEEPTIME)) != ESP_OK)
+    {
+        while (true)
+        {
+            printf ("Error setting sleep time. Value must be out of range.\r\n");
+        }
+    } 
+    esp_deep_sleep_start();
+
+    // If we got here the deep sleep function failed
+    while (true)
+    {
+        printf("Deep sleep initiation failed!\r\n");
+        vTaskDelay(250 / portTICK_PERIOD_MS); // 250ms sleep
+    } 
 }
